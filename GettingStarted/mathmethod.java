c1c3c0358c079a5ff4c0e8e7f5771062e8b3dee0
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2023-10-04 18:55:27 +0300

    Update

---
+++ GettingStarted/mathmethod.java
@@ -0,0 +1,16 @@
+public class mathmethod {
+    public static void main(String[] args)
+    {
+        double x = 3.14;
+        double y = -10;
+
+        double z = Math.max(x,y);
+        double p = Math.min(x,y);
+         double k = Math.abs(y);
+        
+        System.out.println(z);
+        System.out.println(p);
+        System.out.println(k);
+    }
+    
+}
