2634e7ec202e818513cfddd8faa8f8ef16c49f85
NyasimiPhilip <nyasimiphilip@gmail.com>
2023-10-05 22:10:44 +0300

    Update

---
+++ GettingStarted/Stringclassmethod.java
@@ -0,0 +1,18 @@
+public class Stringclassmethod {
+    public static void main(String[] args)
+    {
+        String name = "Bro";
+        boolean result = name.equalsIgnoreCase("bro");
+        System.out.println(result);
+        int result1 = name.length();
+        System.out.println(result1);
+        char character = name.charAt(1);
+        System.out.println(character);
+        int result2 = name.indexOf("rl");
+        System.out.println(result2);
+
+    }
+
+
+
+}
