9fbd478be42b9b5bf0affef785a274f307a6239c
NyasimiPhilip <nyasimiphilip@gmail.com>
2023-10-05 12:06:13 +0300

    Update

---
+++ GettingStarted/switchstatement.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class SwitchStatement {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a day of the week: ");
+        String day = scanner.nextLine();
+
+        switch (day.toLowerCase()) { // Convert input to lowercase for case-insensitive comparison
+            case "sunday":
+                System.out.println("It is Sunday.");
+                break;
+            case "monday":
+                System.out.println("It is Monday.");
+                break;
+            case "tuesday":
+                System.out.println("It is Tuesday.");
+                break;
+            case "wednesday":
+                System.out.println("It is Wednesday.");
+                break;
+            case "thursday":
+                System.out.println("It is Thursday.");
+                break;
+            case "friday":
+                System.out.println("It is Friday.");
+                break;
+            case "saturday":
+                System.out.println("It is Saturday.");
+                break;
+            default:
+                System.out.println("Invalid day entered.");
+                break;
+        }
+
+        scanner.close();
+    }
+}
