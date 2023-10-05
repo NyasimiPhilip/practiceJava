7748450ce99a8471738b19a7f7aac192d6bccede
NyasimiPhilip <nyasimiphilip@gmail.com>
2023-10-05 16:02:01 +0300

    Update

--- GettingStarted/nestedloops.java
+++ GettingStarted/nestedloops.java
@@ -1,11 +1,7 @@
 import java.util.Scanner;
 
 public class nestedloops {
-    /**
-     * @param args
-     */
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int rows;
         int columns;
@@ -15,17 +11,20 @@ public class nestedloops {
         rows = scanner.nextInt();
         System.out.println("Enter No of columns");
         columns = scanner.nextInt();
+
+        // Consume the newline character left in the input stream
+        scanner.nextLine();
+
         System.out.println("Enter symbol");
         symbol = scanner.nextLine();
 
-        for(int i = 1; i<= rows; i++){
-         System.out.println();
-         for(int j= 1; j<= columns; j++)
-         {
-            System.out.print(symbol);
-         }
-         scanner.close();
-      }        
+        for (int i = 1; i <= rows; i++) {
+            System.out.println();
+            for (int j = 1; j <= columns; j++) {
+                System.out.print(symbol);
+            }
+        }
+
+        scanner.close();
     }
-    
 }
