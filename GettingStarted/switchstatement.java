a2afbb93bd172f13cc4a231230fe6c13f030af16
NyasimiPhilip <nyasimiphilip@gmail.com>
2023-10-05 12:20:44 +0300

    Update

--- GettingStarted/switchstatement.java
+++ GettingStarted/switchstatement.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SwitchStatement {
+public class switchstatement {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
