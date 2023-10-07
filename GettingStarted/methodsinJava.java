d2f112d88c683b29776b94a8efccdb1996286b0a
NyasimiPhilip <nyasimiphilip@gmail.com>
2023-10-07 14:27:24 +0300

    Update

--- GettingStarted/methodsinJava.java
+++ GettingStarted/methodsinJava.java
@@ -5,11 +5,18 @@ public class methodsinJava {
         int age = 21;
         hello(name, age);
 
+        System.out.println(add(3 , 4));
+
     }
 
     static void hello(String anything, int anynumber)
     {
         System.out.println("Hello" + anything);
         System.out.println("You are " + anynumber +" years old");
+    }
+
+    static int add(int x, int y)
+    {
+        return x + y;       
     }
 }
