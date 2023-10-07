ea21999247064399b408b118ad5ae190f831b20c
NyasimiPhilip <nyasimiphilip@gmail.com>
2023-10-07 09:59:42 +0300

    Update

--- GettingStarted/MyArrayListExample.java
+++ GettingStarted/MyArrayListExample.java
@@ -1,23 +1,23 @@
 import java.util.ArrayList; // Import ArrayList class from java.util package
 
 public class MyArrayListExample { // Rename your class to something unique
-    
-    public static void main(String[] args, ArrayList<String> arrayList)
+
+    public static void main(String[] args)
     {
         // ArrayList = A resizable array.
         // Elements can be added and removed after compilation phase
         // Store reference data types
-        ArrayList<String> food = new ArrayList<String>();
+        var food = new ArrayList<String>();
 
         food.add("pizza");
         food.add( "hamburger");
         food.add("hotdog");
 
         food.set(0,"sushi");
-        food.remove(2);
-        //food.clear(); to clear an arraylist
+        //food.remove(2);//to remove at a particular index;
+        //food.clear(); //to clear an arraylist
     
-        for(int i= 0; i<arrayList.size(); i++)
+        for(int i= 0; i<food.size(); i++)
          {
         System.out.println(food.get(i));
         }   
