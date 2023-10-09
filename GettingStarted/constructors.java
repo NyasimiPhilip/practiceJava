b6fc8861a593cf5a26627e0459f7a6cc9b397a77
NyasimiPhilip <nyasimiphilip@gmail.com>
2023-10-09 09:24:57 +0300

    Update

---
+++ GettingStarted/constructors.java
@@ -0,0 +1,15 @@
+public class constructors {
+    public static void main(String[] args)
+    {
+        //constructor = special method that is called when an object is instatiated 
+        human human1 = new human("Rick", 65, 70);
+        human human2 = new human("Morty", 16, 50);
+        
+        System.out.println(human1.name);
+        System.out.println(human2.name);
+
+        human1.drink();
+        human2.eat();
+       
+    }
+}
