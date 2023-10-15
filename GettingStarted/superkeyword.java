6641e5044e6ad4338b1ca95a4559e82f500719de
NyasimiPhilip <nyasimiphilip@gmail.com>
2023-10-15 12:40:42 +0300

    Update

---
+++ GettingStarted/superkeyword.java
@@ -0,0 +1,10 @@
+public class superkeyword {
+    public static void main(String[] args)
+    {
+        /*super = keyword refers to the superclass (parent) of an object
+         * very similar to the this keyword
+         */
+        Hero hero1 = new Hero("Batman", 42, "$$$");
+        System.out.println(hero1.power);
+    }
+}
