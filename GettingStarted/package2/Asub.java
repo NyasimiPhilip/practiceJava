3f1e78364d99cca544d0616987cb2e800450fc44
NyasimiPhilip <nyasimiphilip@gmail.com>
2023-10-19 08:24:26 +0300

    Update

---
+++ GettingStarted/package2/Asub.java
@@ -0,0 +1,9 @@
+package package2;
+import package1.*;
+
+public class Asub extends classA{
+    public class classC {
+        String defaultMessage = "This is the default"; 
+     }
+     
+}
---
+++ GettingStarted/package2/classC.java
@@ -0,0 +1,5 @@
+package package2;
+
+public class classC {
+   String defaultMessage = "This is the default"; 
+}
