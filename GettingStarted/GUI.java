55a6a462ad9934d1c9a6571915a316b3494bc11c
NyasimiPhilip <nyasimiphilip@gmail.com>
2023-10-27 15:06:04 +0300

    Update

--- GettingStarted/GUI.java
+++ GettingStarted/GUI.java
@@ -1,8 +1,23 @@
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+
 public class GUI {
     public static void main(String[] args)
     {
 
         /*Jfram = a GUI window to add components to  */
-        JFrame frame = new 
+        JFrame frame = new JFrame();
+        frame.setTitle("Title");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setSize(420,420);
+        frame.setVisible(true);
+
+        ImageIcon image = new ImageIcon("C:\\Users\\HP User\\Desktop\\java\\practicejava\\gettingstarted\\Logo.png");
+        frame.setIconImage(image.getImage());
+        frame.getContentPane().setBackground(new Color(123,50,250));//change color to 
     }
 }
