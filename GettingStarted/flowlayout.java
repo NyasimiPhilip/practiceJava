54ba46c83dac80cb702b0215045b0922f01fffc0
NyasimiPhilip <nyasimiphilip@gmail.com>
2023-10-29 17:07:46 +0300

    Update

--- GettingStarted/flowlayout.java
+++ GettingStarted/flowlayout.java
@@ -1,32 +1,54 @@
 import java.awt.FlowLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Dimension;
 
-public class flowlayout{
+public class flowlayout {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        // Layout Manager = Defines the natural layout for components within a container
 
-		// Layout Manager = Defines the natural layout for components within a container
-		
-		// FlowLayout = 	places components in a row, sized at their preferred size. 
-		//					If the horizontal space in the container is too small,
-		//					the FlowLayout class uses the next available row.
-		
-		JFrame frame = new JFrame();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(500, 500);
-		frame.setLayout(new FlowLayout(FlowLayout.CENTER,0,0));
-		
-		frame.add(new JButton("1"));
-		frame.add(new JButton("2"));
-		frame.add(new JButton("3"));
-		frame.add(new JButton("4"));
-		frame.add(new JButton("5"));
-		frame.add(new JButton("6"));
-		frame.add(new JButton("7"));
-		frame.add(new JButton("8"));
-		frame.add(new JButton("9"));
-		
-		frame.setVisible(true);
-	}
+        // FlowLayout = places components in a row, sized at their preferred size.
+        // If the horizontal space in the container is too small,
+        // the FlowLayout class uses the next available row.
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
+
+        JPanel panel = new JPanel();
+        panel.setPreferredSize(new Dimension(500, 500));
+        panel.setBackground(Color.LIGHT_GRAY);
+        panel.setLayout(new FlowLayout());
+
+        // Create buttons
+        JButton button1 = new JButton("1");
+        JButton button2 = new JButton("2");
+        JButton button3 = new JButton("3");
+        JButton button4 = new JButton("4");
+        JButton button5 = new JButton("5");
+        JButton button6 = new JButton("6");
+        JButton button7 = new JButton("7");
+        JButton button8 = new JButton("8");
+        JButton button9 = new JButton("9");
+
+        // Add buttons to the panel
+        panel.add(button1);
+        panel.add(button2);
+        panel.add(button3);
+        panel.add(button4);
+        panel.add(button5);
+        panel.add(button6);
+        panel.add(button7);
+        panel.add(button8);
+        panel.add(button9);
+
+        // Add the panel to the frame
+        frame.add(panel);
+
+        frame.setVisible(true);
+    }
 }
