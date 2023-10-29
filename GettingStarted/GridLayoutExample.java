2abc96c8d0c6581c42053c63fc62b0efcbbabcf7
NyasimiPhilip <nyasimiphilip@gmail.com>
2023-10-29 17:42:50 +0300

    Update

---
+++ GettingStarted/GridLayoutExample.java
@@ -0,0 +1,47 @@
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Dimension;
+
+public class GridLayoutExample {
+
+    public static void main(String[] args) {
+        // Create the JFrame
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+
+        // Create a JPanel with GridLayout
+        JPanel panel = new JPanel(new GridLayout(3, 3,10,10)); // 3x3 grid
+
+        // Create buttons
+        JButton button1 = new JButton("1");
+        JButton button2 = new JButton("2");
+        JButton button3 = new JButton("3");
+        JButton button4 = new JButton("4");
+        JButton button5 = new JButton("5");
+        JButton button6 = new JButton("6");
+        JButton button7 = new JButton("7");
+        JButton button8 = new JButton("8");
+        JButton button9 = new JButton("9");
+
+        // Add buttons to the panel
+        panel.add(button1);
+        panel.add(button2);
+        panel.add(button3);
+        panel.add(button4);
+        panel.add(button5);
+        panel.add(button6);
+        panel.add(button7);
+        panel.add(button8);
+        panel.add(button9);
+
+        // Add the panel to the frame
+        frame.add(panel);
+
+        // Make the frame visible
+        frame.setVisible(true);
+    }
+}
