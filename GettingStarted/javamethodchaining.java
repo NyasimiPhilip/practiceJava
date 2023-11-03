f40fa648ad0facc5a10bca3cbee99850d87af4de
NyasimiPhilip <nyasimiphilip@gmail.com>
2023-11-03 11:20:59 +0300

    Update

---
+++ GettingStarted/javamethodchaining.java
@@ -0,0 +1,24 @@
+public class javamethodchaining {
+    public static void main(String[] args) {
+        // Method chaining = a common syntax for invoking multiple method calls in OOP to condense code into fewer lines
+        String name = "Bro";
+        name = name.concat("philip   ").toUpperCase();
+        System.out.println(name);
+
+       
+        boolean hasWhiteSpace = containsWhiteSpace(name);
+
+        if (hasWhiteSpace) {
+            System.out.println("The text contains white space.");
+        } else {
+            System.out.println("The text does not contain white space.");
+        }
+    }
+
+    public static boolean containsWhiteSpace(String text) {
+        if (text == null) {
+            return false; // Handle null strings
+        }
+        return text.contains(" "); // Check if the string contains a space character
+    }
+}
